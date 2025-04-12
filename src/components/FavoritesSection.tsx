e0cc5e28fad41d524e42495a70186c433bff79eb
radiolibrevientosur <79158596+radiolibrevientosur@users.noreply.github.com>
2025-04-11 21:24:34 -0400

    Update FavoritesSection.tsx

--- src/components/FavoritesSection.tsx
+++ src/components/FavoritesSection.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { EventCard } from './EventCard';
-import { EventFormData } from '../types';
+import { TaskCard } from './TaskCard';
+import { BirthdayCard } from './BirthdayCard';
+import { EventData, TaskData, BirthdayData } from '../types';
 import { motion } from 'framer-motion';
 
 interface FavoritesSectionProps {
@@ -24,22 +26,39 @@ export const FavoritesSection: React.FC<FavoritesSectionProps> = ({
 }) => {
   return (
     <div className="space-y-6">
-      {/* Mostrar eventos */}
+      {/* Eventos Favoritos */}
       {events.filter(e => e.isFavorite).map(event => (
-        <EventCard key={event.id} event={event} ... />
+        <EventCard
+          key={event.id}
+          event={event}
+          onEdit={() => onEdit(event)}
+          onDelete={() => onDelete('event', event.id)}
+          onToggleReminder={onToggleReminder}
+          onToggleFavorite={() => onToggleFavorite('event', event.id)}
+        />
       ))}
-      
-      {/* Mostrar tareas */}
+
+      {/* Tareas Favoritas */}
       {tasks.filter(t => t.isFavorite).map(task => (
-        <TaskCard key={task.id} task={task} ... />
+        <TaskCard
+          key={task.id}
+          task={task}
+          onDelete={() => onDelete('task', task.id)}
+          onToggleReminder={onToggleReminder}
+          onToggleFavorite={() => onToggleFavorite('task', task.id)}
+        />
       ))}
-      
-      {/* Mostrar cumpleaños */}
+
+      {/* Cumpleaños Favoritos */}
       {birthdays.filter(b => b.isFavorite).map(birthday => (
-        <BirthdayCard key={birthday.id} birthday={birthday} ... />
+        <BirthdayCard
+          key={birthday.id}
+          birthday={birthday}
+          onDelete={() => onDelete('birthday', birthday.id)}
+          onToggleReminder={onToggleReminder}
+          onToggleFavorite={() => onToggleFavorite('birthday', birthday.id)}
+        />
       ))}
     </div>
-  );
-};
   );
 };
